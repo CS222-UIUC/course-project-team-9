7dcdaa2e10ae7a782145339f5724c5417a8bc063
paiz3 <paiz3@illinois.edu>
2022-09-30 21:11:55 -0500

    readme file added

---
+++ readme.md
@@ -0,0 +1,18 @@
+# CS 222 Project Team-9
+
+## Folder Structure
+- website: main folder for the website
+    - website: contains Django and python codes
+    - frontend: contains React project
+
+## Frequent used commands
+### For server
+- python manage.py runserver
+### For React
+- npm run build
+
+## Weekly Review 1
+- `Linter` added
+- `Pytest` added
+- `Coverage badge` added
+- Basic website structure added and tested
--- website/website/settings.py
+++ website/website/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'webproject/build')],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'webproject/build/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
