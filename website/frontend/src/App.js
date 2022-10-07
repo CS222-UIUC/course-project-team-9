7338e2789a8e32d309f845346ab76d80b61eb468
paiz3 <paiz3@illinois.edu>
2022-10-07 16:04:24 -0500

    Title added.

--- website/frontend/src/App.js
+++ website/frontend/src/App.js
@@ -1,15 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Title from './Title';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-      </header>
+      <Title />
     </div>
   );
 }
