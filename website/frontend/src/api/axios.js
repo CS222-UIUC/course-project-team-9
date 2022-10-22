113f7c669458e4bc8b0842b995840840db8d54d9
paiz3 <paiz3@illinois.edu>
2022-10-21 23:22:02 -0500

    debug

--- website/frontend/src/api/axios.js
+++ website/frontend/src/api/axios.js
@@ -2,7 +2,10 @@ import axios from 'axios'
 
 //works on https://jsonplaceholder.typicode.com but
 export const api = axios.create({
-  baseURL: 'http://localhose:8000/api',
+  baseURL: 'http://localhost:8000/api',
+  headers: {
+    'Content-Type': "application/json"
+  } 
 })
 
 export const getPosts = async() => {
