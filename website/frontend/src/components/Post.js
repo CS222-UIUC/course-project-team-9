82562fb184631b15d23cbaa4c9596c65effd2063
paiz3 <paiz3@illinois.edu>
2022-12-06 17:16:50 -0600

    create post enabled

--- website/frontend/src/components/Post.js
+++ website/frontend/src/components/Post.js
@@ -7,7 +7,7 @@ const Post = ({ post }) => {
       <h2>{post.title}</h2>
       <p>Class and Section: {post.class_and_section}</p>
       <p>Description: {post.description}</p>
-      <p>ID: {post.id}</p>
+      {/* <p>ID: {post.id}</p> */}
     </article>
   )
 }
